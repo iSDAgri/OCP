5599eb2e0d92fbb16e28722da1f9ef087ae162f8
mgwalsh <markusgwalsh@gmail.com>
2016-05-30 11:51:41 +0300

    initial comparison to reference values

---
+++ M3_check.R
@@ -0,0 +1,36 @@
+#' Mehlich-3 (M3) compositional data check by comparison to the current AfSIS reference data
+#' M. Walsh, May 2016
+
+# install.packages(c("downloader","compositions"), dependencies=T)
+require(downloader)
+require(compositions)
+
+# Data setup --------------------------------------------------------------
+# Create a data folder in your current working directory
+dir.create("M3_data", showWarnings=F)
+setwd("./M3_data")
+
+# Download AfSIS reference data
+download("https://www.dropbox.com/s/4cfu3crtbcslleq/AfSIS_M3_top.csv.zip?dl=0", "AfSIS_M3_top.csv.zip", mode="wb")
+unzip("AfSIS_M3_top.csv.zip", overwrite=T)
+ref <- read.table("AfSIS_M3_top.csv", header=T, sep=",") ## AfSIS-M3 topsoil reference data
+
+# Copy M3 sample data into ./M3_data ... working directory
+unzip("Sample_M3.csv.zip", overwrite=T) ## note the relevant sample .zip file needs to be copied into your working directory
+samp <- read.table("Sample_M3.csv", header=T, sep=",") ## topsoil sample M3 data
+
+# Compositional data setup
+dat <- rbind(ref, samp)
+vars <- c("B","Mg","P","S","K","Ca","Mn","Fe","Cu","Zn")
+qdat <- na.omit(dat[vars])
+qdat$Fv <- 1000000-rowSums(qdat[vars]) ## calculates "fill value" (Fv), in mg/kg soil
+
+# Centered log ratio (clr) transform
+cvar <- c("B","Mg","P","S","K","Ca","Mn","Fe","Cu","Zn","Fv")
+cdat <- acomp(qdat[cvar])
+clrt <- as.data.frame(clr(cdat)) ## clr transform
+cdat <- cbind(dat[1:2], clrt) ## generates compositional data frame seperated by reference (RS) values
+
+# GLM test ----------------------------------------------------------------
+test <- glm(RS ~ B+Mg+P+S+K+Ca+Mn+Fe+Cu+Zn, family=binomial(link="logit"), cdat)
+summary(test)
