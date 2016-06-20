21af0d96d25b078a3856084354e9f62d0772e935
mgwalsh <markusgwalsh@gmail.com>
2016-06-20 13:45:14 +0300

    documentation update

--- MIR_pred_stats.R
+++ MIR_pred_stats.R
@@ -42,7 +42,7 @@ par(mfrow=c(3,2), mar=c(5,4.5,1,1))
 plo <- 0.25
 phi <- 0.75
 
-# Nitrogen predictions
+# Topsoil Nitrogen predictions
 top_N <- merge(ssid, N, by="SSN")
 top_N <- top_N[which(top_N$depth=="top"), ]
 plot(ecdf(top_N$p50), main="", xlab="N (%)", cex.lab=1.4, ylab="CDF", xlim=c(0,0.3), verticals=T, lty=1, lwd=2, col="red", do.points=F, col.01line = NULL)
@@ -51,7 +51,7 @@ abline(v=quantile(ref$N, probs=phi), h=phi, lty=1, col="grey")
 plot(ecdf(top_N$p05), add=T, verticals=T, lty=2, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 plot(ecdf(top_N$p95), add=T, verticals=T, lty=2, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 
-# Phosphorus predictions
+# Topsoil Phosphorus predictions
 top_P <- merge(ssid, P, by="SSN")
 top_P <- top_P[which(top_P$depth=="top"), ]
 plot(ecdf(top_P$p50), main="", xlab="P (ppm)", cex.lab=1.4, ylab="CDF", xlim=c(0,40), verticals=T, lty=1, lwd=2, col="red", do.points=F, col.01line = NULL)
@@ -60,7 +60,7 @@ abline(v=quantile(ref$P, probs=phi), h=phi, lty=1, col="grey")
 plot(ecdf(top_P$p05), add=T, verticals=T, lty=1, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 plot(ecdf(top_P$p95), add=T, verticals=T, lty=1, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 
-# Potassium predictions
+# Topsoil Potassium predictions
 top_K <- merge(ssid, K, by="SSN")
 top_K <- top_K[which(top_K$depth=="top"), ]
 plot(ecdf(top_K$p50), main="", xlab="K (ppm)", cex.lab=1.4, ylab="CDF", xlim=c(0,300), verticals=T, lty=1, lwd=2, col="red", do.points=F, col.01line = NULL)
@@ -69,7 +69,7 @@ abline(v=quantile(ref$K, probs=phi), h=phi, lty=1, col="grey")
 plot(ecdf(top_K$p05), add=T, verticals=T, lty=1, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 plot(ecdf(top_K$p95), add=T, verticals=T, lty=1, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 
-# Sulfur predictions
+# Topsoil Sulfur predictions
 top_S <- merge(ssid, S, by="SSN")
 top_S <- top_S[which(top_S$depth=="top"), ]
 plot(ecdf(top_S$p50), main="", xlab="S (ppm)", cex.lab=1.4, ylab="CDF", xlim=c(0,40), verticals=T, lty=1, lwd=2, col="red", do.points=F, col.01line = NULL)
@@ -78,7 +78,7 @@ abline(v=quantile(ref$S, probs=phi), h=phi, lty=1, col="grey")
 plot(ecdf(top_S$p05), add=T, verticals=T, lty=1, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 plot(ecdf(top_S$p95), add=T, verticals=T, lty=1, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 
-# Boron predictions
+# Topsoil Boron predictions
 top_B <- merge(ssid, B, by="SSN")
 top_B <- top_B[which(top_B$depth=="top"), ]
 plot(ecdf(top_B$p50), main="", xlab="B (ppm)", cex.lab=1.4, ylab="CDF", xlim=c(0,1), verticals=T, lty=1, lwd=2, col="red", do.points=F, col.01line = NULL)
@@ -87,7 +87,7 @@ abline(v=quantile(ref$B, probs=phi), h=phi, lty=1, col="grey")
 plot(ecdf(top_B$p05), add=T, verticals=T, lty=1, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 plot(ecdf(top_B$p95), add=T, verticals=T, lty=1, lwd=2, col="dark grey", do.points=F, col.01line = NULL)
 
-# Zinc predictions
+# Topsoil Zinc predictions
 top_Zn <- merge(ssid, Zn, by="SSN")
 top_Zn <- top_Zn[which(top_Zn$depth=="top"), ]
 plot(ecdf(top_Zn$p50), main="", xlab="Zn (ppm)", cex.lab=1.4, ylab="CDF", xlim=c(0,4), verticals=T, lty=1, lwd=2, col="red", do.points=F, col.01line = NULL)
