8b288662d3b4ca56d991bfb97b0bf778fb5760c5
mgwalsh <markusgwalsh@gmail.com>
2016-12-30 15:47:27 +0300

    Soil nutrient requirement summaries

--- Soil_nut_reqs.R
+++ Soil_nut_reqs.R
@@ -14,8 +14,7 @@ suppressPackageStartupMessages({
 # SourceURL <- "https://raw.githubusercontent.com/mgwalsh/OCP/blob/master/Spatial_pred_setup.R"
 # source_url(SourceURL)
 
-# Nutrient reference levels (in ppm)
-rN <- 1500
+# Nutrient sufficiency reference levels (in ppm)
 rP <- 30
 rK <- 190
 rS <- 20
@@ -27,7 +26,6 @@ oP <- 2.2913 ## P to P2O5
 oK <- 1.2046 ## K to K2O
 
 # Nutrient mass estimates (kg/ha) in delta notation relative to specified reference levels
-req$dN <- 2*req$BD20*req$N*10000-2*req$BD20*rN ## convert to ppm
 req$dP <- 2*req$BD20*req$P*oP-2*req$BD20*rP ## in P2O5 oxide equivalents
 req$dK <- 2*req$BD20*req$K*oK-2*req$BD20*rK ## in K2O oxide equivalents
 req$dS <- 2*req$BD20*req$S-2*req$BD20*rS
@@ -40,10 +38,6 @@ sid <- leaderCluster(loc, radius=12000, distance="L2")$cluster_id
 req <- cbind(req, sid)
 
 # LMER summaries ----------------------------------------------------------
-# delta N (kg/ha) relative to specified reference level
-dN <- lmer(dN~1+(1|sid), data=req)
-display(dN)
-
 # delta P (kg/ha) relative to specified (P2O5) reference level
 dP <- lmer(dP~1+(1|sid), data=req)
 display(dP)
